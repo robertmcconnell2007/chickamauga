c71153d595ba85c85375c5f8f74eaf993de59574
seancox1990 <seancox1990@40b09f04-00bc-11df-b8ab-b3eb4b1fa784>
2010-01-30 23:09:24 +0000

    retreat stacking is fixed, units can also bump during retreat phase.

--- trunk/chickamauga engine/chickamauga engine/tools/draw tools.cpp
+++ trunk/chickamauga engine/chickamauga engine/tools/draw tools.cpp
@@ -78,6 +78,8 @@ bool showRetreater(mapSuperClass *map,armyClass * attkrs,armyClass *dfndr)
 {
 	battle * tempBattle;	
 	map_node *node;
+	bool nodeFound=false;
+	checkUnitStacks(map,*attkrs,*dfndr);
  	tempBattle=&IH::Instance()->currentBattle;
 	if(tempBattle->attackers.size()>0)//still attackers
 	{
@@ -87,21 +89,21 @@ bool showRetreater(mapSuperClass *map,armyClass * attkrs,armyClass *dfndr)
 		{
 			if(i<3)
 			{
-				if(!node->nodeEdges[i]->upperNode->enemy&&!node->nodeEdges[i]->creek_edge)
+				if(!node->nodeEdges[i]->upperNode->enemy&&!node->nodeEdges[i]->creek_edge&&node->nodeEdges[i]->upperNode->numOfUnits<2)
 				{
 					node->nodeEdges[i]->upperNode->selected=true;
+					nodeFound=true;
 				}
 			}
 			else
 			{
-				if(!node->nodeEdges[i]->lowerNode->enemy&&!node->nodeEdges[i]->creek_edge)
+				if(!node->nodeEdges[i]->lowerNode->enemy&&!node->nodeEdges[i]->creek_edge&&node->nodeEdges[i]->lowerNode->numOfUnits<2)
 				{
 					node->nodeEdges[i]->lowerNode->selected=true;
+					nodeFound=true;
 				}
 			}
 		}
-		map->clearEnemy();
-		return true;
 	}
 	else if(tempBattle->defenders.size()>0)
 	{
@@ -111,25 +113,151 @@ bool showRetreater(mapSuperClass *map,armyClass * attkrs,armyClass *dfndr)
 		{
 			if(i<3)
 			{
-				if(!node->nodeEdges[i]->upperNode->enemy&&!node->nodeEdges[i]->creek_edge)
+				if(!node->nodeEdges[i]->upperNode->enemy&&!node->nodeEdges[i]->creek_edge&&node->nodeEdges[i]->upperNode->numOfUnits<2)
 				{
 					node->nodeEdges[i]->upperNode->selected=true;
+					nodeFound=true;
 				}
 			}
 			else
 			{
-				if(!node->nodeEdges[i]->lowerNode->enemy&&!node->nodeEdges[i]->creek_edge)
+				if(!node->nodeEdges[i]->lowerNode->enemy&&!node->nodeEdges[i]->creek_edge&&node->nodeEdges[i]->lowerNode->numOfUnits<2)
 				{
 					node->nodeEdges[i]->lowerNode->selected=true;
+					nodeFound=true;
 				}
 			}
 		}
-		map->clearEnemy();
-		return true;
 	}
 	else
 	{
 		IH::Instance()->retreatCalled=false;
 		return false;
 	}
+	if(nodeFound)
+	{
+		map->clearEnemy();
+		return true;
+	}
+	else//funky crap about to go down
+	{
+		if(tempBattle->attackers.size()>0)
+		{
+			for(int i=0; i<6; i++)
+			{
+				if(i<3)
+				{
+					if(!node->nodeEdges[i]->upperNode->enemy&&node->nodeEdges[i]->upperNode->numOfUnits==2)
+					{
+						for(int k=0; k<attkrs->currentSize; k++)
+						{
+							if(attkrs->armyArray[k]->getX()==node->nodeEdges[i]->upperNode->col&&attkrs->armyArray[k]->getY()==node->nodeEdges[i]->upperNode->row)
+							{
+								nodeFound=true;
+								tempBattle->attackers.push_back(attkrs->armyArray[k]);
+							}
+						}
+					}
+					if(nodeFound)
+					{
+						break;
+					}
+				}
+				else
+				{
+					if(!node->nodeEdges[i]->lowerNode->enemy&&node->nodeEdges[i]->lowerNode->numOfUnits==2)
+					{
+						for(int k=0; k<attkrs->currentSize; k++)
+						{
+							if(attkrs->armyArray[k]->getX()==node->nodeEdges[i]->lowerNode->col&&attkrs->armyArray[k]->getY()==node->nodeEdges[i]->lowerNode->row)
+							{
+								nodeFound=true;
+								tempBattle->attackers.push_back(attkrs->armyArray[k]);
+							}
+						}
+					}
+					if(nodeFound)
+					{
+						break;
+					}
+				}
+			}
+		}
+		else if(tempBattle->defenders.size()>0)
+		{
+			for(int i=0; i<6; i++)
+			{
+				if(i<3)
+				{
+					if(!node->nodeEdges[i]->upperNode->enemy&&node->nodeEdges[i]->upperNode->numOfUnits==2)
+					{
+						for(int k=0; k<dfndr->currentSize; k++)
+						{
+							if(dfndr->armyArray[k]->getX()==node->nodeEdges[i]->upperNode->col&&dfndr->armyArray[k]->getY()==node->nodeEdges[i]->upperNode->row)
+							{
+								nodeFound=true;
+								tempBattle->defenders.push_back(dfndr->armyArray[k]);
+							}
+						}
+					}
+					if(nodeFound)
+					{
+						break;
+					}
+				}
+				else
+				{
+					if(!node->nodeEdges[i]->lowerNode->enemy&&node->nodeEdges[i]->lowerNode->numOfUnits==2)
+					{
+						for(int k=0; k<dfndr->currentSize; k++)
+						{
+							if(dfndr->armyArray[k]->getX()==node->nodeEdges[i]->lowerNode->col&&dfndr->armyArray[k]->getY()==node->nodeEdges[i]->lowerNode->row)
+							{
+								nodeFound=true;
+								tempBattle->defenders.push_back(dfndr->armyArray[k]);
+							}
+						}
+					}
+					if(nodeFound)
+					{
+						break;
+					}
+				}
+			}
+		}
+	}
+	if(nodeFound)
+	{
+		map->clearEnemy();
+		return true;
+	}
+	else//if all else fails...unit says FML
+	{
+		if(tempBattle->attackers.size()>0)
+		{
+			for(int k=0; k<attkrs->currentSize; k++)
+			{
+				if(attkrs->armyArray[k]==tempBattle->attackers.back())
+				{
+					attkrs->moveUnit(attkrs->armyArray[k],MUFField,MUTKilled);
+					tempBattle->attackers.pop_back();
+					break;
+				}
+			}
+		}
+		else if(tempBattle->defenders.size()>0)
+		{
+			for(int k=0; k<dfndr->currentSize; k++)
+			{
+				if(dfndr->armyArray[k]==tempBattle->defenders.back())
+				{
+					dfndr->moveUnit(dfndr->armyArray[k],MUFField,MUTKilled);
+					tempBattle->attackers.pop_back();
+					break;
+				}
+			}
+		}
+
+	}
+	return true;
 }
